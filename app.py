46a8512c2b9fc568395cd0758856e31c6577b652
vnallam09 <nallamothuvenkat09@gmail.com>
2025-06-30 17:15:26 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,22 @@
+import plotly.express as px
+from shiny.express import input, ui
+from shinywidgets import render_plotly
+import palmerpenguins  # This package provides the Palmer Penguins dataset
+
+
+
+# Use the built-in function to load the Palmer Penguins dataset
+penguins_df = palmerpenguins.load_penguins()
+
+ui.page_opts(title="Teja's Penguin Data",fillable=True)
+
+ui.page_opts(title="Filling layout", fillable=True)
+with ui.layout_columns():
+
+    @render_plotly
+    def plot1():
+        return px.histogram(px.data.tips(), y="tip")
+
+    @render_plotly
+    def plot2():
+        return px.histogram(px.data.tips(), y="total_bill")
